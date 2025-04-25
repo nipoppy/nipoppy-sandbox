5e6f4421d5b559199825be1e76eefe7f9bf067ee
ThomasHMAC <39802916+ThomasHMAC@users.noreply.github.com>
2025-04-25 14:18:59 -0400

    [ENH] Added xcp_d-0.7.3 pipeline (#3)

--- boutiques_descriptors/get_descriptor.py
+++ boutiques_descriptors/get_descriptor.py
@@ -21,99 +21,7 @@ TYPE_NUMBER = "Number"
 TYPE_FLAG = "Flag"
 TYPE_STRING = "String"
 
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(
-        description=(
-            "Generate a Boutiques descriptor for a given tool."
-            "\n- Only works for CLIs built with argparse."
-            "\n- Need to make sure to checkout the right version: git checkout tags/<TAG>."
-            "\n  Then do pip install"
-            "\n- This script needs to be modified for new tools."
-            "\n- The descriptor might need some manual tweaking too."
-        ),
-        formatter_class=argparse.RawTextHelpFormatter,
-    )
-
-    parser.add_argument(
-        "tool_name", type=str, help="Name of the tool to generate the descriptor for."
-    )
-    parser.add_argument(
-        "--module-name",
-        type=str,
-        help="Name of the module to import (default: same as tool name).",
-    )
-    parser.add_argument(
-        "--tool-version",
-        type=str,
-        help="Version of the tool to use (default: obtained from the package).",
-    )
-
-    args = parser.parse_args()
-
-    tool_name = args.tool_name
-    module_name = args.module_name
-    tool_version = args.tool_version
-
-    if module_name is None:
-        module_name = tool_name
-
-    try:
-        module = import_module(module_name)
-        if tool_name in ["fmriprep", "mriqc"]:
-            path_parser_module = module_name
-            for submodule_name in ("cli", "parser"):
-                path_parser_module = f"{path_parser_module}.{submodule_name}"
-                parser_module = import_module(path_parser_module)
-            parser = parser_module._build_parser()
-        elif tool_name in ["halfpipe"]:
-            path_parser_module = f"{module_name}.cli.parser"
-            parser_module = import_module(path_parser_module)
-            parser = parser_module.build_parser()
-        elif tool_name in ["heudiconv"]:
-            path_parser_module = f"{module_name}.cli.run"
-            parser_module = import_module(path_parser_module)
-            parser = parser_module.get_parser()
-        elif tool_name in ["dcm2bids", "dcm2bids_helper"]:
-            path_parser_module = f"{module_name}.cli.{tool_name}"
-            parser_module = import_module(path_parser_module)
-            parser = parser_module._build_arg_parser()
-        elif tool_name in ["bidsmapper", "bidseditor", "bidscoiner"]:
-            path_parser_module = f"{module_name}.cli._{tool_name}"
-            parser_module = import_module(path_parser_module)
-            parser = parser_module.get_parser()
-        elif tool_name in ["qsiprep"]:
-            # older versions
-            try:
-                path_parser_module = f"{module_name}.cli.run"
-                parser_module = import_module(path_parser_module)
-                parser = parser_module.get_parser()
-            # newer versions
-            except AttributeError:
-                path_parser_module = f"{module_name}.cli.parser"
-                parser_module = import_module(path_parser_module)
-                parser = parser_module._build_parser()
-        else:
-            raise RuntimeError(
-                f"Unable to get parser for {tool_name}. Check this script and make necessary changes."
-            )
-    except Exception as exception:
-        print(
-            f"Error while importing modules dynamically. Make sure that {module_name} has been installed "
-            "in this environment and that the import for the parser is correct."
-        )
-        raise exception
-
-    if tool_version is None:
-        try:
-            tool_version = module.__version__
-        except AttributeError:
-            tool_version = import_module(f"{module_name}.version").__version__
-
-    fpath_out = f"{tool_name}-{tool_version}.json"
-
-    print(f"tool_version: {tool_version}")
-
+def process_descriptor(parser, tool_name, tool_version):
     new_descriptor = boutiques.creator.CreateDescriptor(
         parser,
         execname=tool_name,
@@ -139,6 +47,13 @@ if __name__ == "__main__":
         "qsiprep": [("bids_filters", "bids_filter_file"), ("memory_gb", "mem")],
         "mriqc": [("memory_gb", "mem")],
         "dcm2bids_helper": [("overwrite", "force")],
+        "xcp_d": [
+            ("bids_filters", "bids_filter_file"),
+            ("memory_gb", "mem_gb"),
+            ("bandpass_filter", "disable_bandpass_filter"),
+            ("process_surfaces", "warp_surfaces_native2std"),
+            ("dcan_qc", "skip_dcan_qc")
+        ] 
     }
     I_INPUT_BBR = None  # may need to be deleted/replaced for fMRIPrep
     for i_input, input_object in enumerate(new_descriptor.descriptor[INPUTS_FIELD]):
@@ -230,6 +145,28 @@ if __name__ == "__main__":
             # descriptor builder only keeps the first one
             if input_object[ID_FIELD] == "force_bbr":
                 I_INPUT_BBR = i_input
+
+        elif tool_name == "xcp_d":
+            if input_object[ID_FIELD] in [
+                "disable_bandpass_filter",
+                "skip_dcan_qc"
+            ]:
+                input_object[TYPE_FIELD] = TYPE_FLAG
+                print(
+                    f'Setting "{TYPE_FIELD}" field to {TYPE_FLAG} for {input_object[ID_FIELD]}'
+                )
+                if DEFAULT_VALUE_FIELD in input_object:
+                    print(
+                        f"Deleting default value for {input_object[ID_FIELD]} ({input_object[DEFAULT_VALUE_FIELD]})"
+                    )
+                    del input_object[DEFAULT_VALUE_FIELD]
+            # type
+            if input_object[ID_FIELD] in ["head_radius", "min_coverage"]:
+                input_object[TYPE_FIELD] = TYPE_NUMBER
+                print(
+                    f'Setting "{TYPE_FIELD}" field to {TYPE_NUMBER} for {input_object[ID_FIELD]}'
+                )
+
         elif tool_name == "mriqc":
             if input_object[ID_FIELD] in ["version"]:
                 input_object[TYPE_FIELD] = TYPE_FLAG
@@ -273,7 +210,7 @@ if __name__ == "__main__":
                 )
 
         # Nipreps tools 'verbose_count' input
-        if tool_name in ["fmriprep", "mriqc", "qsiprep"]:
+        if tool_name in ["fmriprep", "mriqc", "qsiprep", "xcp_d"]:
             if input_object[ID_FIELD] == "verbose_count":
                 input_object[CHOICES_FIELD] = ["-v", "-vv", "-vvv"]
                 print(f'Setting "choices" field for {input_object[ID_FIELD]}')
@@ -340,9 +277,110 @@ if __name__ == "__main__":
             COMMAND_LINE_FIELD
         ].replace(existing_key, f"{existing_key} {new_key}")
 
+    return new_descriptor
+
+def get_descriptor(tool_name, module_name, tool_version):
+
+    if module_name is None:
+        module_name = tool_name
+
+    try:
+        module = import_module(module_name)
+        if tool_name in ["fmriprep", "mriqc", "xcp_d"]:
+            path_parser_module = module_name
+            for submodule_name in ("cli", "parser"):
+                path_parser_module = f"{path_parser_module}.{submodule_name}"
+                parser_module = import_module(path_parser_module)
+            parser = parser_module._build_parser()
+        elif tool_name in ["halfpipe"]:
+            path_parser_module = f"{module_name}.cli.parser"
+            parser_module = import_module(path_parser_module)
+            parser = parser_module.build_parser()
+        elif tool_name in ["heudiconv"]:
+            path_parser_module = f"{module_name}.cli.run"
+            parser_module = import_module(path_parser_module)
+            parser = parser_module.get_parser()
+        elif tool_name in ["dcm2bids", "dcm2bids_helper"]:
+            path_parser_module = f"{module_name}.cli.{tool_name}"
+            parser_module = import_module(path_parser_module)
+            parser = parser_module._build_arg_parser()
+        elif tool_name in ["bidsmapper", "bidseditor", "bidscoiner"]:
+            path_parser_module = f"{module_name}.cli._{tool_name}"
+            parser_module = import_module(path_parser_module)
+            parser = parser_module.get_parser()
+        elif tool_name in ["qsiprep"]:
+            # older versions
+            try:
+                path_parser_module = f"{module_name}.cli.run"
+                parser_module = import_module(path_parser_module)
+                parser = parser_module.get_parser()
+            # newer versions
+            except AttributeError:
+                path_parser_module = f"{module_name}.cli.parser"
+                parser_module = import_module(path_parser_module)
+                parser = parser_module._build_parser()
+        else:
+            raise RuntimeError(
+                f"Unable to get parser for {tool_name}. Check this script and make necessary changes."
+            )
+    except Exception as exception:
+        print(
+            f"Error while importing modules dynamically. Make sure that {module_name} has been installed "
+            "in this environment and that the import for the parser is correct."
+        )
+        raise exception
+
+    if tool_version is None:
+        try:
+            tool_version = module.__version__
+        except AttributeError:
+            tool_version = import_module(f"{module_name}.version").__version__
+
+    fpath_out = f"{tool_name}-{tool_version}.json"
+
+    print(f"tool_version: {tool_version}")
+
+    new_descriptor = process_descriptor(parser, tool_name, tool_version)
+
     print("===== Saving =====")
     new_descriptor.save(fpath_out)
     print(fpath_out)
 
     print("===== Validating ===== ")
     print(boutiques.bosh(["validate", fpath_out]))
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description=(
+            "Generate a Boutiques descriptor for a given tool."
+            "\n- Only works for CLIs built with argparse."
+            "\n- Need to make sure to checkout the right version: git checkout tags/<TAG>."
+            "\n  Then do pip install"
+            "\n- This script needs to be modified for new tools."
+            "\n- The descriptor might need some manual tweaking too."
+        ),
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+
+    parser.add_argument(
+        "tool_name", type=str, help="Name of the tool to generate the descriptor for."
+    )
+    parser.add_argument(
+        "--module-name",
+        type=str,
+        help="Name of the module to import (default: same as tool name).",
+    )
+    parser.add_argument(
+        "--tool-version",
+        type=str,
+        help="Version of the tool to use (default: obtained from the package).",
+    )
+
+    args = parser.parse_args()
+
+    tool_name = args.tool_name
+    module_name = args.module_name
+    tool_version = args.tool_version
+
+    get_descriptor(tool_name, module_name, tool_version)
